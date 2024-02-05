0626daf2bac481efb7dac544a80b2c20659aa638
Myles Henehan <mylesjhenehan98@gmail.com>
2024-02-05 16:41:25 +0000

    Work in Progress - to be continued

---
+++ bank.py
@@ -0,0 +1,6 @@
+from __future__ import division
+amountone = int(input("Please enter an amount in cent: "))
+amounttwo = int(input("Please enter another amount in cent: "))
+sum = amountone + amounttwo
+euro = sum / 100
+print("The total sum is " + str(sum / 100))
