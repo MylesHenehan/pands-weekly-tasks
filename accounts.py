8adb0b057901bea46f0ca8304475f73c3701a454
Myles Henehan <mylesjhenehan98@gmail.com>
2024-02-14 23:24:04 +0000

    First attempt at extra part of task

--- accounts.py
+++ accounts.py
@@ -6,3 +6,16 @@
 accountnumber = input("Please enter your 10 digit account number: ")
 lastfourdigits = accountnumber[6:10]
 print(f"Account Number: XXXXXX{lastfourdigits}")
+
+# If we were to modify the program to allow for numbers of any length, we would need to know the length of the account number before printing.
+# By using length -4 to length as the range, we can still make sure there are 4 digits at the end.
+# However, we will also need to ensure that the number of Xs is correct, so we should add a value Xnumber which multiplies X by the number of digits in the account number
+
+accountnumber = input("Please enter your account number: ")
+lastdigit = len(accountnumber)
+firstdigit = lastdigit - 4
+
+Xnumber = firstdigit * "X"
+lastfourdigits = accountnumber[firstdigit:lastdigit]
+
+print(f"Account Number: {Xnumber}{lastfourdigits}")
