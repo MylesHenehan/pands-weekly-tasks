31ff1f34def513d8b842ad6e51aa378fe99a444f
Myles Henehan <mylesjhenehan98@gmail.com>
2024-03-14 22:03:24 +0000

    Mistake fixed in week 4 exercise

--- collatz.py
+++ collatz.py
@@ -1,6 +1,6 @@
 # Program that asks the user to input any positive integer and outputs the successive values of the following calculation: 
 # if it is even, divide it by 2, but if it is odd, multiply it by 3 and add 1
-# this should stop when it reaches 0
+# this should stop when it reaches 1
 
 # Author: Myles Henehan
 
