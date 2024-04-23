9d129c15bd3c27053964e188b9732d7a66aa6909
Myles Henehan <mylesjhenehan98@gmail.com>
2024-04-24 00:25:48 +0100

    Looking over weekly tasks

--- bank.py
+++ bank.py
@@ -4,7 +4,18 @@
 #Author: Myles Henehan
 
 amountone = int(input("Please enter an amount in cent: "))
+# User is prompted to input their first integer
 amounttwo = int(input("Please enter another amount in cent: "))
+# User is prompted to input a second integer
 sum = amountone + amounttwo
+# Program then adds these integers together and calls it the sum - this will be the total number of cents.
+# Since we have to be conscious of the inaccuracy of floats, we now want to split what comes before and after the decimal point.
 euro = sum // 100
-print(f"The total sum is €{sum // 100}.{sum % 100}")
+# We can get the euro amount by dividing by 100 (without remainder)
+cents = sum % 100
+# Then we get the cents by getting the remainder.
+
+print(f"The total sum is €{euro}.{cents}")
+
+
+#Note to self: Could there be a more simple way using 10 to the power?
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,6 @@
+# Weekly Task 1
+#Instructions: This file should contain a python program that displays Hello World! when it is run.
+
 print("Hello World")
+
+# This is a simple built-in print function.
